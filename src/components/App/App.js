f0eeb2cd6e341bcd0b54ff0750e62aef20fd8c39
tanchyk <tanchyk.bohdan@gmail.com>
2020-07-13 18:36:14 +0300

    Yelp API request is shown to a user

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BuisnessList';
-
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-const businesses = [business, business, business, business, business, business];
+import Yelp from '../../until/Yelp';
 
 class App extends React.Component {
-    searchYelp(term, location, sortBy) {
-        console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
-    }
+   constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <div className="App">
-                <h1>ravenous</h1>
-                <SearchBar searchYelp={this.searchYelp}/>
-                <BusinessList businesses={businesses}/>
-            </div>
-        );
-    }
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
 };
 
 export default App;
